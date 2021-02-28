7027aa7c39345e38ef45f3f12e7fca855ab037da
Mahirweyves <yvesmahirwe1@gmail.com>
2021-02-27 20:38:47 -0800

    fixed config & views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,11 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Notify_production"
+  
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     
